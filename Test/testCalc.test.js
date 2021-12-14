aa0a44de2a2ced9c019820c5ccd92eac0a41d75f
SergioV19 <sergiovar19@gmail.com>
2021-12-14 18:13:10 -0500

    commit con para realizar un testeo correcto

--- Test/testCalc.test.js
+++ Test/testCalc.test.js
@@ -2,7 +2,7 @@ const funcionalidad = require('../Functions/Calculadora')
 
 describe('prueba de calculadora', ()=>{
     test('prueba', ()=>{
-        const result  = funcionalidad( '4', '2', "/")
-        expect(result).toBe(1)
+        const result  = funcionalidad( '3', '2', "*")
+        expect(result).toBe(6)
     })
 })
